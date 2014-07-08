7fc066ced5c7a982ca37f167362eaeff0b005297
Jainy Zau <jainyzau@gmail.com>
2014-07-08 10:48:34 -0400

    Add blog source code for the first time

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "jz", password: "jz", only: :destroy
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  def test
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    render @comment
+  end
+end
